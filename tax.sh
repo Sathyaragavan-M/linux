73a3bd989769b59c4d34ec94e403eabfc2e19c35
ragav <ragav@gmail.com>
2024-08-05 04:31:56 +0000

    shell in feature

---
+++ tax.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "enter your salary : "
+read sum
+if [ $sum -lt 10000 ]
+then
+	echo "no tax"
+elif [ $sum -lt 25000 ]
+then
+	echo "5% tax for your salary $sum"
+else
+	echo "10% tax for your salary $sum"
+fi
+
